11271546255da5b9928bdcf280abfd42d5f71ae7
Todd Buiten <todd@buiten.com>
2024-12-21 13:44:56 -0800

    Add PROPANE_TIMEOUT

--- lora-cgm-sender.ino
+++ lora-cgm-sender.ino
@@ -83,6 +83,8 @@ U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // Ea
 #include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
 #endif
+
+#define PROPANE_TIMEOUT (3600 * 6)
 #endif
 
 // Setting the clock...
@@ -210,7 +212,7 @@ void drawBorder(int32_t x, int32_t y, int32_t w, int32_t h, int32_t color) {
 long httpsTaskHighWaterMark = LONG_MAX;
 volatile long mgPerDl = -1;
 int propaneLevel = -1;
-ExpirationTimer propaneExpirationTimer = ExpirationTimer(millis() + 86400000);
+ExpirationTimer propaneExpirationTimer = ExpirationTimer(millis() + (PROPANE_TIMEOUT * 1000));
 void vHttpsTask(void* pvParameters) {
   while (true) {
     JsonDocument doc;
@@ -244,7 +246,7 @@ void vHttpsTask(void* pvParameters) {
       Serial.println("If not then the login credentials are bad");
     }
 
-    if (propaneExpirationTimer.isExpired(86400000)) {
+    if (propaneExpirationTimer.isExpired(PROPANE_TIMEOUT * 1000)) {
       if (callApi("https://ws.otodatanetwork.com/neevoapp/v1/DataService.svc/GetAllDisplayPropaneDevices", "propane", &doc)) {
         propaneLevel = (int) doc[0]["Level"];
         Serial.print("Propane level = ");
@@ -362,9 +364,9 @@ void displayPropaneLevel() {
 
     sprintf(displayBuffer, "%d", propaneLevel);
 #if defined(DISPLAY_TYPE_ST7735_128_160)
-    rightJustify(displayBuffer, FONT_NUMBER, FONT_SIZE_PROPANE, TFT_GREEN, 200, 9, 2 * 16);
+    rightJustify(displayBuffer, FONT_NUMBER, FONT_SIZE_PROPANE, TFT_GREEN, 160, 9, 2 * 16);
 #elif defined(DISPLAY_TYPE_ILI9488_480_320)
-    rightJustify(displayBuffer, FONT_NUMBER, FONT_SIZE_PROPANE, TFT_GREEN, 200, 9, 2 * 16);
+    rightJustify(displayBuffer, FONT_NUMBER, FONT_SIZE_PROPANE, TFT_GREEN, 160, 9, 2 * 16);
 #endif
 
     oldPropaneLevel = propaneLevel;
